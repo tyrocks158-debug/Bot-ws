020efcea8be0524eb5b99354ab65e6098027e17b
tyrocks158-debug <tyrocks158@gmail.com>
2026-09-25 21:52:27 +0530

    Add files via upload

---
+++ databases.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    "activity": {"url": "https://activity-e16b3-default-rtdb.firebaseio.com", "keys": []},
+    "m05960864": {"url": "https://m05960864-6140e-default-rtdb.firebaseio.com", "keys": []},
+    "m93374762": {"url": "https://m93374762-8c0f6-default-rtdb.firebaseio.com", "keys": []},
+    "master80221": {"url": "https://master80221-753fb-default-rtdb.firebaseio.com", "keys": []},
+    "myadmin": {"url": "https://myadmin-38635-default-rtdb.firebaseio.com", "keys": []},
+    "pm-kisan-20": {"url": "https://pm-kisan-20-vgg-default-rtdb.firebaseio.com", "keys": ["AIzaSyCoZC16SJuhmzNKgBLfGpSW540P7pGsctE"]},
+    "rajkumar8822556644": {"url": "https://rajkumar8822556644-407f5-default-rtdb.firebaseio.com", "keys": []},
+}
